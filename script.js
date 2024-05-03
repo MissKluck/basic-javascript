2969e268c6887f4acf8947ae4972acb945c59624
misskluck <sigrid.b.kluck@gmail.com>
2024-05-03 12:44:17 +0200

    updated info about tinkaton

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-const statement = "lars" === "Lars"
+import { Tinkaton } from "./tinkaton.js";
+const imageElement = document.getElementById("Regular-image");
+console.log(imageElement);
 
-if (statement) {
-    // console.log("It's true")
-} else {
-    //console.log("It's false")
-}
+imageElement.src = Tinkaton.sprites.front_default;
 
-const age = 28
+const nameElement = document.getElementById("eng-name");
+console.dir(nameElement);
+
+nameElement.textContent = Tinkaton.species.name;
+
+// const statement = "lars" === "Lars";
+
+//if (statement) {
+// console.log("It's true")
+//} else {
+//console.log("It's false")
+//}
+
+const age = 28;
 
 if (age < 18) {
-    //console.log("You're too young to enter")
-} else if (age < 30){
-    //console.log("Welcome")
+  //console.log("You're too young to enter")
+} else if (age < 30) {
+  //console.log("Welcome")
 } else {
-    //console.log("You're too old")
+  //console.log("You're too old")
 }
 
 switch (1) {
-    case 1:
-        //console.log("Doing task #1")
-        break;
+  case 1:
+    //console.log("Doing task #1")
+    break;
 
-    case 2:
-        //console.log("Doing task #2")
-        break;
+  case 2:
+    //console.log("Doing task #2")
+    break;
 
-    case 3:
-        //console.log("Doing task #3")
-        break;
+  case 3:
+    //console.log("Doing task #3")
+    break;
 
-    default:
-        //console.log("Default task")
-        break;
+  default:
+    //console.log("Default task")
+    break;
 }
 
 //console.log("Finished")
